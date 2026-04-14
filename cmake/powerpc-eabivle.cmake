bee20e3dd4f734bd5dad802b4e9204c7be41fdb2
Sergo_101 <a9152870050@gmail.com>
2026-04-14 18:53:50 +0300

    remove absolute paths from build scripts

--- cmake/powerpc-eabivle.cmake
+++ cmake/powerpc-eabivle.cmake
@@ -3,19 +3,13 @@ set(CMAKE_SYSTEM_PROCESSOR          arm)
 
 # Some default GCC settings
 # arm-none-eabi- must be part of path environment
-set(GCC_PATH    "C:/buildtools/NXP/build_tools/powerpc-eabivle-4_9/bin/")
 set(TOOLCHAIN_PREFIX                powerpc-eabivle-)
-# set(FLAGS                           "-fdata-sections -ffunction-sections -Wl,--gc-sections")
-# set(CPP_FLAGS                       "${FLAGS} -fno-rtti -fno-exceptions -fno-threadsafe-statics")
 
-# set(CMAKE_C_FLAGS                   ${FLAGS})
-# set(CMAKE_CXX_FLAGS                 ${CPP_FLAGS})
-
-set(CMAKE_C_COMPILER                ${GCC_PATH}${TOOLCHAIN_PREFIX}gcc.exe)
+set(CMAKE_C_COMPILER                ${TOOLCHAIN_PREFIX}gcc)
 set(CMAKE_ASM_COMPILER              ${CMAKE_C_COMPILER})
-set(CMAKE_CXX_COMPILER              ${GCC_PATH}${TOOLCHAIN_PREFIX}g++.exe)
-set(CMAKE_OBJCOPY                   ${GCC_PATH}${TOOLCHAIN_PREFIX}objcopy.exe)
-set(CMAKE_SIZE                      ${GCC_PATH}${TOOLCHAIN_PREFIX}size.exe)
+set(CMAKE_CXX_COMPILER              ${TOOLCHAIN_PREFIX}g++)
+set(CMAKE_OBJCOPY                   ${TOOLCHAIN_PREFIX}objcopy)
+set(CMAKE_SIZE                      ${TOOLCHAIN_PREFIX}size)
 
 set(CMAKE_EXECUTABLE_SUFFIX_ASM     ".elf")
 set(CMAKE_EXECUTABLE_SUFFIX_C       ".elf")
